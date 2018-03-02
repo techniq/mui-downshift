b6fd7fd5147b12a831dcd14c9fb20da0ee2de914
caub <cyril.auburtin@gmail.com>
2018-03-02 20:10:04 +0100

    stories/fetch too

--- stories/fetch.js
+++ stories/fetch.js
@@ -12,14 +12,16 @@ const items = starwarsNames.map((text, value) => ({ text, value }));
 storiesOf('Fetch', module)
   .add('basic', () => (
     <MockFetch items={items} url="https://example.com/">
-      {({ loading, data, error, fetch, clearData }) => (
+      {({
+        loading, data, error, fetch, clearData
+      }) => (
         <MuiDownshift
           items={data && data.items}
           loading={loading}
           onStateChange={changes => {
-            if (changes.hasOwnProperty('inputValue')) {
+            if (typeof changes.inputValue === 'string') {
               clearData();
-              fetch(`https://example.com/?q=${changes.inputValue}`)
+              fetch(`https://example.com/?q=${changes.inputValue}`);
             }
           }}
         />
@@ -29,7 +31,9 @@ storiesOf('Fetch', module)
 
   .add('no initial fetch', () => (
     <MockFetch items={items} url="https://example.com/" manual>
-      {({ loading, data, error, fetch, clearData }) => (
+      {({
+        loading, data, error, fetch, clearData
+      }) => (
         <MuiDownshift
           items={data && data.items}
           getListItem={({ getItemProps, item }) => (
@@ -51,11 +55,11 @@ storiesOf('Fetch', module)
           includeFooter={loading}
           loading={loading}
           onStateChange={changes => {
-            if (changes.hasOwnProperty('inputValue')) {
+            if (typeof changes.inputValue === 'string') {
               clearData();
-              fetch(`https://example.com/?q=${changes.inputValue}`)
-            } else if (changes.hasOwnProperty('isOpen') && data == null) {
-              fetch(`https://example.com/`)
+              fetch(`https://example.com/?q=${changes.inputValue}`);
+            } else if (changes.isOpen !== undefined && data == null) {
+              fetch('https://example.com/');
             }
           }}
         />
@@ -64,13 +68,15 @@ storiesOf('Fetch', module)
   ))
 
   .add('infinte loading', () => (
-    <MockFetch items={items} url="https://example.com/?startIndex=0&stopIndex=20" 
-      onDataChange={(newData, currentData = { total: 0, items: [] }) => {
-        return { total: newData.total, items: [...currentData.items, ...newData.items] }
-      }}
+    <MockFetch
+      items={items}
+      url="https://example.com/?startIndex=0&stopIndex=20"
+      onDataChange={(newData, currentData = { total: 0, items: [] }) => ({ total: newData.total, items: [...currentData.items, ...newData.items] })}
       manual
     >
-      {({ loading, data, error, fetch, request, clearData }) => (
+      {({
+        loading, data, error, fetch, request, clearData
+      }) => (
         <MuiDownshift
           items={data && data.items}
           getInfiniteLoaderProps={({ downshiftProps }) => ({
@@ -84,9 +90,9 @@ storiesOf('Fetch', module)
               params.set('startIndex', startIndex);
               params.set('stopIndex', stopIndex + 1);
               url.search = params.toString();
-              return fetch(url.toString())
-            }})
-          }
+              return fetch(url.toString());
+            }
+          })}
           getListItem={({ getItemProps, item }) => (
             item ? (
               <ListItem button {...getItemProps()}>
@@ -106,11 +112,11 @@ storiesOf('Fetch', module)
           includeFooter={loading}
           loading={loading}
           onStateChange={changes => {
-            if (changes.hasOwnProperty('inputValue')) {
+            if (typeof changes.inputValue === 'string') {
               clearData();
-              fetch(`https://example.com/?q=${changes.inputValue}&startIndex=0&stopIndex=20`, null, { ignorePreviousData: true })
-            } else if (changes.hasOwnProperty('isOpen') && data == null) {
-              fetch(`https://example.com/?startIndex=0&stopIndex=20`)
+              fetch(`https://example.com/?q=${changes.inputValue}&startIndex=0&stopIndex=20`, null, { ignorePreviousData: true });
+            } else if (changes.isOpen !== undefined && data == null) {
+              fetch('https://example.com/?startIndex=0&stopIndex=20');
             }
           }}
         />
@@ -119,13 +125,15 @@ storiesOf('Fetch', module)
   ))
 
   .add('paginated loading', () => (
-    <MockFetch items={items} url="https://example.com/?startIndex=0&stopIndex=10" 
-      onDataChange={(newData, currentData = { total: 0, items: [] }) => {
-        return { total: newData.total, items: [...currentData.items, ...newData.items] }
-      }}
+    <MockFetch
+      items={items}
+      url="https://example.com/?startIndex=0&stopIndex=10"
+      onDataChange={(newData, currentData = { total: 0, items: [] }) => ({ total: newData.total, items: [...currentData.items, ...newData.items] })}
       manual
     >
-      {({ loading, data, error, fetch, request, clearData }) => {
+      {({
+        loading, data, error, fetch, request, clearData
+      }) => {
         const hasMoreData = data && data.items.length < data.total;
 
         return (
@@ -136,16 +144,17 @@ storiesOf('Fetch', module)
             showEmpty
             getListItemKey={(rowIndex, columnIndex) => {
               if (data && data.items && data.items[rowIndex]) {
-                return data.items[rowIndex].text
+                return data.items[rowIndex].text;
               } else if (loading) {
-                return 'loading'
+                return 'loading';
               } else if (hasMoreData) {
-                return 'hasMoreData'
-              } else {
-                return 'noItemsFound'
+                return 'hasMoreData';
               }
+                return 'noItemsFound';
             }}
-            getListItem={({ getItemProps, item, index, downshiftProps }) => (
+            getListItem={({
+              getItemProps, item, index, downshiftProps
+            }) => (
               item ? (
                 <ListItem button {...getItemProps()}>
                   <ListItemText primary={item.text} secondary="Test" />
@@ -155,18 +164,22 @@ storiesOf('Fetch', module)
                   <ListItemText primary={<span style={{ fontStyle: 'italic' }}>Loading...</span>} />
                 </ListItem>
               ) : hasMoreData ? (
-                <ListItem button style={{ backgroundColor: '#ccc' }} onClick={() => {
+                <ListItem
+                  button
+                  style={{ backgroundColor: '#ccc' }}
+                  onClick={() => {
                   downshiftProps.setHighlightedIndex(null);
 
                   const url = new URL(request.url);
                   const params = new URLSearchParams(url.search);
                   const currentStartIndex = Number(params.get('startIndex'));
-                  const currentStopIndex = Number(params.get('stopIndex'))
+                  const currentStopIndex = Number(params.get('stopIndex'));
                   params.set('startIndex', currentStartIndex + 10);
                   params.set('stopIndex', currentStopIndex + 10);
                   url.search = params.toString();
-                  fetch(url.toString())
-                }}>
+                  fetch(url.toString());
+                }}
+                >
                   <ListItemText primary={<span style={{ color: '#fff' }}>Load more items</span>} />
                 </ListItem>
               ) : (
@@ -176,15 +189,15 @@ storiesOf('Fetch', module)
               )
             )}
             onStateChange={changes => {
-              if (changes.hasOwnProperty('inputValue')) {
+              if (typeof changes.inputValue === 'string') {
                 clearData();
-                fetch(`https://example.com/?q=${changes.inputValue}&startIndex=0&stopIndex=10`, null, { ignorePreviousData: true })
-              } else if (changes.hasOwnProperty('isOpen') && data == null) {
-                fetch(`https://example.com/?startIndex=0&stopIndex=10`)
+                fetch(`https://example.com/?q=${changes.inputValue}&startIndex=0&stopIndex=10`, null, { ignorePreviousData: true });
+              } else if (changes.isOpen !== undefined && data == null) {
+                fetch('https://example.com/?startIndex=0&stopIndex=10');
               }
             }}
           />
-        )
+        );
       }}
     </MockFetch>
-  ))
+  ));
