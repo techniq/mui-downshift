168d0c89d512dc8a620947f1b9acbcee25b8de2e
Sean Lynch <techniq35@gmail.com>
2018-04-07 13:59:18 -0400

    Fix starwarsName after text => label rename

--- stories/components/StarWarsSelect.js
+++ stories/components/StarWarsSelect.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { all as starwarsNames } from 'starwars-names';
 import MuiDownshift from '../../src';
 
-const items = starwarsNames.map((text, value) => ({ text, value }));
+const items = starwarsNames.map((label, value) => ({ label, value }));
 
 export default class StarWarsSelect extends Component {
   state = {
